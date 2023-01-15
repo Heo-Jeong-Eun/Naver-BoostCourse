b955569115b973ea8301bf55594b81f79ca716ed
J <1001l1000l1110@gmail.com>
2023-01-15 20:25:26 +0900

    cs-study ai basic
    01-15

--- week 1./team-mission/Q1.py
+++ week 1./team-mission/Q1.py
@@ -1,9 +1,42 @@
 # Q1. num_list에서 홀수 데이터만 출력하는 함수를 작성
 # output : [1, 5, 7, 15, 29]
 
+# A. 
+#   1. 홀수만 저장할 리스트를 선언, 
+#   2. num_list를 for문으로 반복했을 때 
+#   3. i == num_list의 원소 값을 2로 나누었을 때 나머지가 1인 경우
+#   4. 리스트에 요소를 추가하는 함수인 append()를 사용하여 해당 i값을 result 리스트에 추가한다.  
+
+
 num_list = [1, 5, 7, 15, 16, 22, 28, 29]
 
-# def get_odd_num(num_list):
-    # todo
+def get_odd_num(num_list):
+    result = [] # 빈 리스트 선언
+    for i in num_list: # num_list까지 반복, 리스트 모든 요소를 반복문으로 훑고 싶을 때는 in 뒤에 리스트를 지정하면 된다.
+        if i % 2 == 1: # 홀수 판별식, 2로 나누었을 때 나머지가 1인 경우 
+            result.append(i) # result라는 리스트에 append() 명령어로 i를 추가해준다. 
+    return result
 
-# print(get_odd_num(num_list))
+print(get_odd_num(num_list))
+
+
+# error code !
+# output : [1, 5, 7, 15, 16, 22, 28, 29]
+"""
+# A. 
+#   짝수인 경우 del을 이용해 삭제하고 num_list를 출력하려고 했으나 
+#   del, clear, remove, pop을 사용해도 삭제되지 않아서 실패했다. 
+#   인덱스를 보고 요소를 삭제하는 명령어라서 에러가 발생하는 것인지 어떤 이유인지 아직 찾지 못했다. 
+
+def get_odd_num(num_list):
+    result = []
+    for i in num_list:
+        if i % 2 == 0:
+            result.remove(i)
+            result.clear(i)
+            result.pop(i)
+            del i 
+    return num_list 
+
+print(get_odd_num(num_list))
+"""
--- week 1./team-mission/Q2.py
+++ week 1./team-mission/Q2.py
@@ -1,9 +1,26 @@
 # Q2. string 문장을 받아 단어를 역순으로 출력하는 함수를 작성
 # ouput : Where there is a will there is a way
- 
+
+# A. 
+#   1. 공백 기준으로 나눠서 리스트에 저장 한 뒤 
+#   2. 역순으로 정렬
+#   3. 공백을 구분자로 다시 합친 뒤 출력한다. 
+
 sentence = "way a is there will a is there Where"
 
-# def reverse_sentence(sentence):
-    # todo
+def reverse_sentence(sentence): 
+    rev_sentence = list(sentence.split(' ')) 
+    # rev_sentence 변수에 list() 함수를 이용해 sentence 문자열을 리스트에 저장하는데, 
+    # 이때 split()을 사용해 리스트에 저장할 때 기준, 즉 구분자를 추가한다.
+    # 구분자를 공백으로 주어서 way, a, is, there, will, a, is, there, Where 
+    # 다음과 같이 rev_sentence 리스트에 삽입 되도록 한다. 
+    # return (rev_sentence) -> 이 부분 주석을 지우고 출력해보면 확인이 가능하다. 
 
-# print(reverse_sentence(sentence))
+    return ' '.join(reversed(rev_sentence))
+    # 나누어진 문자열을 역순으로 정렬하는 reversed()함수로 rev_sentence를 감싸주고
+    # join()을 통해 문자열을 결합한다. 
+    # 이때 나누었던 결과를 바탕으로 결합이 가능하고, join의 . 앞에 공백으로 구분자를 추가해주면
+    # 구분자를 기준으로 결합을 한다. 
+    # join은 리스트, 튜플, 딕셔너리에서 사용이 가능하다. 
+
+print(reverse_sentence(sentence)) 
