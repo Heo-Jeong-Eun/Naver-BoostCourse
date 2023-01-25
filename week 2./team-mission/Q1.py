4668f7a468070af24ffb2693ec3ccd96a03f62ef
J <1001l1000l1110@gmail.com>
2023-01-26 00:25:56 +0900

    cs-study ai basic
    01-26

--- week 2./team-mission/Q1.py
+++ week 2./team-mission/Q1.py
@@ -3,17 +3,31 @@
 # output : 62.5
 
 # A. 
-#   1. 
+#   1. 인스턴스를 private, 즉 __mid, __final과 같은 형태로 선언 하고
+#   2. @property 데코레이터를 활용 -> getter, setter를 구현해 데이터를 저장한다. 
 
-class Score():
-    # __mid = 0 # private variable, 외부에서 접근이 불가능하다. 
-    # __final = 0
+#   접근 제어자 : 객체의 프로퍼티 접근을 제한하는 명령어
+#   @property : 객체의 프로퍼티를 보호해주는 함수이다.
+#   getter : 값을 가져오는 method, setter : 값을 저장하는 method
+class Score:
+    def __init__(self, __mid, __final):
+        self.__mid = __mid
+        self.__final = __final
+        # __mid = 0 # private variable, 외부에서 접근이 불가능하다. 
+        # __final = 0
 
-    def mid(self):
+    @property # 데코레이터 @property를 사용, 값을 읽을 때마다 getter/function을 따로 호출 X, property 이름으로 접근 O
+    def mid(self): # self : 인스턴스 그 자체, 객체 자기 자신을 참조하는 매개변수
         return self.__mid
-
+    @mid.setter # setter 선언 방법 : method 이름 + .setter
+    def mid(self, value):
+        self.__mid = value # self.__mid에 value 값 저장
+    @property
     def final(self):
         return self.__final
+    @final.setter
+    def final(self, value):
+        self.__final = value
 
-score = Score(50, 75)
-print((score.mid + score.final) / 2)
+score = Score(50, 75) 
+print((score.mid + score.final) / 2) # 인스턴스.속성 형식으로 값을 가져온다. 
--- week 2./team-mission/Q2.py
+++ week 2./team-mission/Q2.py
@@ -3,14 +3,28 @@
 # output : gas 2 small
 
 # A. 
-#   1. 
+#   1. fuel, wheels 값을 가지고 있는 Car 클래스(부모)를 제작
+#   2. size 값을 가지고 있는 Bike 클래스(자식)에 상속한 후 출력
 
-class Car():
+class Car(object): # class에 부모가 없으면, object를 기본으로 삼는다. 생략 가능
     def __init__(self, fuel, wheels):
         self.fuel = fuel
         self.wheels = wheels
-
-# class Bike():
     
+    def car(self): # class Car는 self.fuel과 self.wheels 값을 return 한다. 
+        return (self.fuel, self.wheels)
+
+class Bike(Car): # 부모 class 
+    def __init__(self,fuel, wheels, size):
+        super().__init__(fuel, wheels) 
+        # super와 __init__에 대한 호출 확인
+        # super().init() : 다른 클래스의 속성, method를 자동으로 호출, 해당 클래스에서 사용이 가능하도록 해준다. 
+        # 상속 받은 클래스 내에서 해당 method를 새롭게 다시 구현해주면
+        # 부모 클래스와 다른 기능을 수행하도록 만들 수 있다. 
+        self.size = size # 변수 size 추가 
+
+    def bike(self):
+        return (self.fuel, self.wheels, self.size)
+
 bike = Bike("gas", 2, "small")
 print(bike.fuel, bike.wheels, bike.size)
